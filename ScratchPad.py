5450fc34b4f5b1c3a21cf77f139fdebfa4e971b9
sxs149331 <sxs149331@utdallas.edu>
2017-09-05 14:23:06 -0500

    Minor Foramtting update

--- ScratchPad.py
+++ ScratchPad.py
@@ -1,17 +1,25 @@
-from io import open
-import json
+# from io import open
+# import json
+#
+# cameoRulesList = json.load(open("CameoRule.json","r", encoding='UTF-8'))
+# count = 0
+# for x in cameoRulesList:
+#     if x["source"] != "CAMEO2":
+#         print x["ruleText"]
+#         count += 1
+#
+# print count
+#
+# testFileList = json.load(open("Word.json","r", encoding='UTF-8'))
+#
+# for key in testFileList[0]:
+#     print key
+#
 
-cameoRulesList = json.load(open("CameoRule.json","r", encoding='UTF-8'))
-count = 0
-for x in cameoRulesList:
-    if x["source"] != "CAMEO2":
-        print x["ruleText"]
-        count += 1
 
-print count
+import schedule
 
-testFileList = json.load(open("Word.json","r", encoding='UTF-8'))
+def job():
+    print "Done in every 10 minutes"
 
-for key in testFileList[0]:
-    print key
-
+schedule.every().day.at("14:15").do()
