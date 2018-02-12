41d0e21b9c3e2afed14d193601c503e10ffed174
sxs149331 <sxs149331@utdallas.edu>
2018-02-12 13:56:28 -0600

    Updated Code for Arabic and other languages, documentation missing

--- ScratchPad.py
+++ ScratchPad.py
@@ -1,5 +1,10 @@
 ##encoding= utf-8
 
 arabic_part = "عمل_Test ]عة"
-
-print arabic_part, "[010] Test", arabic_part
+full_sent = arabic_part + " TEST [010]"
+print full_sent
+words = full_sent.split(" ")
+print words[0].decode('utf-8'), words[1].decode('utf-8')
+print [x.decode("utf-8") for x in words]
+# print arabic_part, "[010] " + "Test"
+# print "m"+ arabic_part+ "[010] Test"+ arabic_part#, , arabic_part
